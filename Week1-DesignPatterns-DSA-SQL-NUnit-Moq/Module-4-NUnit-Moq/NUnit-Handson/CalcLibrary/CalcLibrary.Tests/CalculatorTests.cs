5bab5afc08a02ba7c6956754bb662ad26dae07c2
prasanthi2906 <mannavaprashanthi@gmail.com>
2026-07-17 23:51:51 +0530

    Added remaining mandatory Week 1 modules

---
+++ Week1-DesignPatterns-DSA-SQL-NUnit-Moq/Module-4-NUnit-Moq/NUnit-Handson/CalcLibrary/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private SimpleCalculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator.AllClear();
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 5, 10)]
+        [TestCase(100, 200, 300)]
+        public void Addition_ShouldReturnExpectedResult(
+            double a,
+            double b,
+            double expected)
+        {
+            double result = calculator.Addition(a, b);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
